8e480c340fb34d7b9a139969db0917434f6428fe
gg思密达 <31328777+h1004015780@users.noreply.github.com>
2019-06-11 11:35:25 +0800

    Update 223.py

--- win32/223.py
+++ win32/223.py
@@ -5,7 +5,6 @@ with open('D:/123.txt','r') as s:
     str = s.readlines()
     s.close()
 #list获取到文本的参数
-# list=[x.strip() for x in str]
 list = [int(x) for x in str]
 a1,a2,a3,a4 = list[0],list[1],list[2],list[3]
 a = 420
